8d55320ec2ac50124afc5ba6c129dd3e5827607c
Jared Beck <jared@jaredbeck.com>
2014-12-30 10:57:50 -0500

    Update method signature of url_for
    Changed in rails 4.2

--- lib/subdomain_fu/url_rewriter.rb
+++ lib/subdomain_fu/url_rewriter.rb
@@ -3,7 +3,20 @@ require 'action_dispatch/routing/route_set'
 module ActionDispatch
   module Routing
     class RouteSet #:nodoc:
-      def url_for_with_subdomains(options, path_segments=nil)
+
+      # Known Issue: Monkey-patching `url_for` is error-prone.
+      # For example, in rails 4.2, the method signature changed from
+      #
+      #     def url_for(options)
+      #
+      # to
+      #
+      #     def url_for(options, route_name = nil, url_strategy = UNKNOWN)
+      #
+      # Is there an alternative design using composition or class
+      # inheritance?
+
+      def url_for_with_subdomains(options, *args)
         if SubdomainFu.needs_rewrite?(options[:subdomain], (options[:host] || (@request && @request.host_with_port))) || options[:only_path] == false
           options[:only_path] = false if SubdomainFu.override_only_path?
           options[:host] = SubdomainFu.rewrite_host_for_subdomains(options.delete(:subdomain), options[:host] || (@request && @request.host_with_port))
@@ -12,7 +25,9 @@ module ActionDispatch
         end
         url_for_without_subdomains(options)
       end
+
       alias_method_chain :url_for, :subdomains
+
     end
   end
 end
